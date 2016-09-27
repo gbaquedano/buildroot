e7caec3cc297ecc60f64aaec533561c0b063194a
Gabriel <g.baquedano.89@gmail.com>
2016-09-27 20:19:36 +0200

    RPI(0,1,2,3): Bump to kernel 4.4.22
    linux-headers: bump 4.7.x series to 4.7.4
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    
    # Conflicts:
    #	package/linux-headers/Config.in.host (reverted from commit cc9ba60b862b15d3fc3e74f6518f89156257c3a1)
    linux-headers: bump 3.{2, 14}.x and 4.4.x series
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    linux-headers: remove deprecated versions
    
    Linux headers 3.19.x and 4.0.x have been deprecated for a while now, so
    it's time to remove them.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    linux-headers: bump 4.{4, 7}.x series
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = ad8608c08b122b2c228dba0ff5070d6e9519faf5
+RPI_FIRMWARE_VERSION = 91c30e0181e1cee0281133d630ebe5e9d4e00615
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
